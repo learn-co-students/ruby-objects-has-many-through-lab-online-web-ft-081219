ec9bae75a49ad4894855dce6f02d3891fe48c33b
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-05 01:13:41 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+class Genre 
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+  @name = name
+  @@all << self
+end
+
+ def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def artists 
+    Song.all.map do |genre|
+      genre.artist
+  end 
+end
+end
