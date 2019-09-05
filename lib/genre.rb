9d56b996e7cf664da4995bd6986d13c38c2239f2
David Ingram <david@ingramaviation.com>
2019-09-05 02:55:46 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+class Genre 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists
+    self.songs.map {|artist| artist.genre == self}
+  end 
+
+end 
